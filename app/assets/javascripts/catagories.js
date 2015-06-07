bfe008549149de415484cfbad3d8180b82bc6784
Chris Bohl <chrisrbohl@gmail.com>
2015-06-06 21:43:20 -0500

    changed file

--- app/assets/javascripts/catagories.js
+++ app/assets/javascripts/catagories.js
@@ -1,15 +1,27 @@
-var Results = angular.module('FieldSearch', ['ngAnimate', 'anguFixedHeaderTable']);
+// var Results = angular.module('FieldSearch', ['ngAnimate', 'anguFixedHeaderTable']);
 
-Results.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
-    $http.get('../../resource_files/scc.json')
-    .success(function(response) {
-        $scope.rules = response;
-    });
-}]);
+// Results.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+//     $http.get('../../resource_files/scc.json')
+//     .success(function(response) {
+//         $scope.rules = response;
+//     });
+// }]);
 
-Results.controller('DetailsCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
-    $http.get('../../resource_files/scc.json')
-    .success(function(response) {
-        $scope.rules = response;
-        $scope.whichItem = $routeParams.itemId;
-});
+// Results.controller('DetailsCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+//     $http.get('../../resource_files/scc.json')
+//     .success(function(response) {
+//         $scope.rules = response;
+//         $scope.whichItem = $routeParams.itemId;
+// });
+
+
+
+var restauranteur = angular.module('restauranteur', [])
+
+restauranteur.config(['$routeProvider', ($routeProvider) -&gt;
+  $routeProvider.
+    otherwise({
+      templateUrl: '../templates/home.html',
+      controller: 'HomeCtrl'
+    })
+])
